e7f996e8f140ec9d6ac6d262b9a14fea701aea89
Anthony Gress <the.techx.guy@gmail.com>
2025-02-18 23:15:33 -0500

    chunk bundle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,23 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 2022
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        const modulePath = id.split('node_modules/')[1];
+                        const topLevelFolder = modulePath.split('/')[0];
+                        if (topLevelFolder !== '.pnpm') {
+                            return topLevelFolder;
+                        }
+                        const scopedPackageName = modulePath.split('/')[1];
+                        const chunkName = scopedPackageName.split('@')[scopedPackageName.startsWith('@') ? 1 : 0];
+                        return chunkName;
+                    }
+                }
+            }
+        }
     }
 });
