0a9b0731cfb012292da38fa062f9c75ec6040efd
Anthony Gress <the.techx.guy@gmail.com>
2025-02-18 20:25:04 -0500

    fix dragging on mobile with icons for real, blur navbar

--- frontend/src/components/ResponsiveAppBar.tsx
+++ frontend/src/components/ResponsiveAppBar.tsx
@@ -64,7 +64,7 @@ export const ResponsiveAppBar = ({ editMode, setEditMode, customTitle }: Props) 
     };
 
     return (
-        <AppBar position='fixed' sx={{ backgroundColor: COLORS.TRANSPARENT_GRAY }}>
+        <AppBar position='fixed' sx={{ backgroundColor: COLORS.TRANSPARENT_GRAY, backdropFilter: 'blur(6px)' }}>
             <Container sx={{ margin: 0, padding: 0, minWidth: '100%' }}>
                 <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
                     {/* Desktop */}
