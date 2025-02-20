0e3bd83586725eb0fda37b52621ed1948b4e7483
Anthony Gress <the.techx.guy@gmail.com>
2025-02-20 00:52:07 -0500

    fix weather size ipad

--- frontend/src/components/widgets/WeatherWidget.tsx
+++ frontend/src/components/widgets/WeatherWidget.tsx
@@ -205,7 +205,7 @@ export const WeatherWidget: React.FC = () => {
                     {/* 1 Day */}
                     {renderCurrentWeatherItem()}
                     {/* 5 Day */}
-                    <Grid container gap={{ xs: 3, sm: 2, md: 5 }} sx={{ px: 2 }}>
+                    <Grid container gap={{ xs: 3, sm: 2, md: 4, lg: 5 }} sx={{ px: 2 }}>
                         { forecastDays > 1 && renderWeatherItem() }
                     </Grid>
                 </Grid>
